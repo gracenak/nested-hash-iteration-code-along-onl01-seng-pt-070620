0e47f5e70eca4adf4608a39fe8621f925d1142b3
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-17 18:53:46 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,13 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    if person == "Freddy Mercury"
-      contact_details_hash.each do |attribute, data|
-        binding.pry
-      end
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      binding.pry
     end
   end
+end
   
   binding.pry
 end
